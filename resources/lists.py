6a1ad614661dc1e61d9d64d015ddfb85bc44a7f1
Griffin Delgado <griffinelijahd@gmail.com>
2019-12-01 21:31:09 -0600

    get lists belonging to logged in user route functioning

--- resources/lists.py
+++ resources/lists.py
@@ -18,3 +18,15 @@ def create_list():
 	new_list_dict = model_to_dict(new_list)
 	return jsonify(data=new_list_dict, status={'code': 201, 'message':'successfully created list'}), 201
 
+#display all lists belonging to logged in user
+@lists.route('/myLists', methods=['GET'])
+def current_users_lists():
+	#look for all lists that have a userid matching the logged in users id
+	try:
+		this_users_list_instances = models.List.select().where(models.List.user_id == current_user.id)
+		#turn all posts found into arr of dicts
+		this_users_list_dicts = [model_to_dict(lists) for lists in this_users_list_instances]
+		return jsonify(data=this_users_list_dicts, status={'code': 200, 'message': 'successfully retrieved all of your lists'}), 200
+
+	except models.DoesNotExist:
+		return jsonify(data={}, status={'code': 401, 'message': 'error retrieving lists'}), 401
