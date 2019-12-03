bbfa59a80718b38e6d324799aba5a36141a1ca00
Griffin Delgado <griffinelijahd@gmail.com>
2019-12-03 15:57:34 -0600

    can now create Item with scraped data captured in variables

--- models.py
+++ models.py
@@ -1,8 +1,12 @@
+import sqlite3
 import datetime
 from peewee import *
 from flask_login import UserMixin
+from scraping_scripts.farfetch import *
+from playhouse.shortcuts import model_to_dict
 
 DATABASE = SqliteDatabase('price_drop.sqlite')
+
 
 class User(UserMixin, Model):
 	email = CharField(unique = True)
@@ -22,18 +26,26 @@ class List(Model):
 
 class Item(Model):
 	url = CharField()
+	name = CharField()
 	image = CharField()
 	original_price = CharField()
 	disc_price = CharField()
 	notif_preference = CharField()
 	created_date = DateTimeField(default=datetime.datetime.now)
-	list_id = ForeignKeyField(List, backref='items')
+	list_id = ForeignKeyField(List, backref='items', null=True)
 
 	class Meta:
 		database = DATABASE
+
 
 def initialize():
 	DATABASE.connect()
 	DATABASE.create_tables([User, List, Item], safe=True)
 	print('TABLES CREATED')
+	#creating item object with data pulled from variables iin the farfetch.py file
+	item_object = Item(url = url, name = name_text, image = image_text, original_price = price_text, disc_price = price_text, notif_preference = '25')
+	#turn into dict before creating record in db
+	item_dict = model_to_dict(item_object)
+	#create db entry in the Item table with spread op info
+	Item.create(**item_dict)
 	DATABASE.close()
--- scraping_scripts/farfetch.py
+++ scraping_scripts/farfetch.py
@@ -1,23 +1,39 @@
 import bs4 as bs
 # import requests
 import urllib.request
+import sqlite3
+conn = sqlite3.connect('price_drop.sqlite', check_same_thread=False)
+curs = conn.cursor()
+
 #this is the Url we will scrape 
-sauce = urllib.request.urlopen('https://www.farfetch.com/shopping/men/prada-palm-print-shirt-item-14752178.aspx').read()
+url = ('https://www.farfetch.com/shopping/men/prada-palm-print-shirt-item-14752178.aspx')
+sauce = urllib.request.urlopen(url).read()
 
 #Load the plain html data into a variable
 soup = bs.BeautifulSoup(sauce, 'html.parser')
 
 #this gets the original price for and if applicable discounted price of an item on farfetch
 price = soup.find('div', {'class': '_844eda'})
-print(price.text.strip())
+# print(price.text.strip())
+price_text = price.get_text()
 
 #this will get the name of the item
 name = soup.find('span', {'class': '_b4693b'})
-print(name.text.strip())
+# print(name.text.strip())
+name_text = name.get_text()
+
 
 #this will get the image src for item
-image = soup.findAll('img', {'class': '_221e30'})
-print(image[0])
+image = soup.find('img', {'class': '_221e30'})
+# print(image[0])
+image_text = image.get_text()
+
+
+
+
+
+
+
 
 
 
