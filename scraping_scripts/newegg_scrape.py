ad68780c3a3cf869d6de910b155841502505a360
Griffin Delgado <griffinelijahd@gmail.com>
2019-12-03 11:34:17 -0600

    script to get image from farfetch item complete

--- scraping_scripts/newegg_scrape.py
+++ scraping_scripts/newegg_scrape.py
@@ -2,7 +2,7 @@ import bs4 as bs
 # import requests
 import urllib.request
 #this is the Url we will scrape 
-sauce = urllib.request.urlopen('https://www.farfetch.com/shopping/men/prada-palm-print-shirt-item-14752178.aspx').read()
+sauce = urllib.request.urlopen('https://www.farfetch.com/shopping/men/a-cold-wall-faded-effect-logo-print-hoodie-item-13790443.aspx?storeid=9306').read()
 
 #Load the plain html data into a variable
 # plain_html = requests.get(sauce)
@@ -19,4 +19,9 @@ print(price.text.strip())
 title = soup.find('span', {'class': '_b4693b'})
 print(title.text.strip())
 
+# image = soup.findAll('img')
+# print(image)
+image = soup.findAll('img', {'class': '_221e30'})
+print(image)
 
+# for img in soup.findAll('img', )
