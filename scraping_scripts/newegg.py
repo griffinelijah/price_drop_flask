75282ebc5686a3e5cc21507b3e9ab7a5290e89e0
Griffin Delgado <griffinelijahd@gmail.com>
2019-12-04 15:04:41 -0600

    correctly scrapes newegg original price

--- scraping_scripts/newegg.py
+++ scraping_scripts/newegg.py
@@ -11,7 +11,7 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome(executable_path='/Users/griffindelgado/Downloads/chromedriver')
 
 #this is the url that will be parsed
-newegg_url = ('https://www.target.com/p/lg-55-39-39-class-4k-uhd-smart-oled-tv-oled55b9pua/-/A-52328755')
+newegg_url = ('https://www.newegg.com/msi-geforce-rtx-2070-super-rtx-2070-super-ventus-oc/p/N82E16814137438?Item=N82E16814137438&cm_sp=Homepage_BS-_-P2_14-137-438-_-12042019')
 
 #5 second delay to load page and scrape before returning error 
 delay = 5
@@ -19,10 +19,12 @@ delay = 5
 #this makes a simple get request to the url
 driver.get(newegg_url)
 
-
-element= WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.h-padding-b-default > div.h-text-red > div:nth-child(3)')))
-print('element is ready!')
-element_text = element.text
-print(element_text)
-html_of_interest = driver.execute_script('return arguments[0].innerHTML',element)
-sel_soup=BeautifulSoup(html_of_interest, 'html.parser')
+try:	
+	element= WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#landingpage-price > div > div > ul > li.price-current')))
+	print('element is ready!')
+	price_text = element.text
+	print(price_text)
+	html_of_interest = driver.execute_script('return arguments[0].innerHTML',element)
+	sel_soup=BeautifulSoup(html_of_interest, 'html.parser')
+except:
+	print('error scraping resource')
--- scraping_scripts/target.py
+++ scraping_scripts/target.py
@@ -7,12 +7,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+#this is the path to where the chrome driver.exe file is
 driver = webdriver.Chrome(executable_path='/Users/griffindelgado/Downloads/chromedriver')
 
+#this is the url that will be parsed
 target_url = ('https://www.target.com/p/lg-55-39-39-class-4k-uhd-smart-oled-tv-oled55b9pua/-/A-52328755')
 
+#5 second delay to load page and scrape before returning error 
 delay = 5
 
+#this makes a simple get request to the url
 driver.get(target_url)
 
 try:
