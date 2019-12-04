5451ab2cd824b16682c86639ce52d90700e7219a
Griffin Delgado <griffinelijahd@gmail.com>
2019-12-04 14:42:30 -0600

    tested to make sure all items on website can be scraped properly

--- scraping_scripts/target.py
+++ scraping_scripts/target.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome(executable_path='/Users/griffindelgado/Downloads/chromedriver')
 
-target_url = ('https://www.target.com/p/vizio-d-series-40-class-39-50-diag-1080p-full-array-led-smart-hdtv-d40f-g9/-/A-53855744')
+target_url = ('https://www.target.com/p/lg-55-39-39-class-4k-uhd-smart-oled-tv-oled55b9pua/-/A-52328755')
 
 delay = 5
 
@@ -22,18 +22,11 @@ print(element_text)
 html_of_interest = driver.execute_script('return arguments[0].innerHTML',element)
 sel_soup=BeautifulSoup(html_of_interest, 'html.parser')
 
-
-
-
-    # viewport > div:nth-child(4) > div > div.Row-uds8za-0.gnKDVb > div.Col-favj32-0.h-padding-h-default.h-padding-t-tight.styles__StyledCol-sc-1n8m629-12.eiisQZ > div.h-padding-b-default > div.h-text-red > div:nth-child(3)
-
 #url to be scraped / will later change to a variable that will hold url user inputs
 sauce = urllib.request.urlopen(target_url).read()
 
 #load all html in variable
 soup = bs.BeautifulSoup(sauce, 'html.parser')
-
-#scrapes price
 
 
 #scrapes item name 
