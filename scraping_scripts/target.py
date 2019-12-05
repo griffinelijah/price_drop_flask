1e4ec413ce5c1dcb23eab867a3c157e29e77a124
Griffin Delgado <griffinelijahd@gmail.com>
2019-12-05 11:31:49 -0600

    refactored selenium scripts for price

--- scraping_scripts/target.py
+++ scraping_scripts/target.py
@@ -22,24 +22,24 @@ driver.get(target_url)
 #this block will get us the original price 
 try:
 	#tell the webDriver to wait to scrape until the element is located by it's css selector. If the element has not been rendered and found it won't begin the scrape
-	element_disc_price= WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.h-padding-b-default > div.h-text-red > div:nth-child(3)')))
+	element_orig_price= WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.h-padding-b-default > div.h-text-red > div:nth-child(3)')))
 	print('element is ready!')
 	#turn all element data into text
-	element__disc_text = element_disc_price.text
-	print(element__disc_text)
+	element__orig_text = element_orig_price.text
+	print(element__orig_text)
 	#execute script once element is rendered
-	html_of_interest = driver.execute_script('return arguments[0].innerHTML',element,)
+	html_of_interest = driver.execute_script('return arguments[0].innerHTML',element_orig_price,)
 	sel_soup=BeautifulSoup(html_of_interest, 'html.parser')
 except:#if error present 
 	print('error scraping resource')
 
 #second try except to get discounted price 
 try:
-	element_orig_price= WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#viewport > div:nth-child(4) > div > div.Row-uds8za-0.gnKDVb > div.Col-favj32-0.h-padding-h-default.h-padding-t-tight.styles__StyledCol-sc-1n8m629-12.eiisQZ > div.h-padding-b-default > div.h-text-red > div.h-text-bold.style__PriceFontSize-gob4i1-0.eLdTvF')))
+	element_disc_price= WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#viewport > div:nth-child(4) > div > div.Row-uds8za-0.gnKDVb > div.Col-favj32-0.h-padding-h-default.h-padding-t-tight.styles__StyledCol-sc-1n8m629-12.eiisQZ > div.h-padding-b-default > div.h-text-red > div.h-text-bold.style__PriceFontSize-gob4i1-0.eLdTvF')))
 	print('element is ready!')
-	element_orig_text = element_orig_price.text
-	print(element_orig_text)
-	html_of_interest = driver.execute_script('return arguments[0].innerHTML',element_orig_price,)
+	disc_price_text = element_disc_price.text
+	print(disc_price_text)
+	html_of_interest = driver.execute_script('return arguments[0].innerHTML',element_disc_price,)
 	sel_soup=BeautifulSoup(html_of_interest, 'html.parser')
 except:
 	print('error scraping resource')
@@ -55,7 +55,7 @@ soup = bs.BeautifulSoup(sauce, 'html.parser')
 
 #scrapes item name 
 name = soup.find('h1', {'class': 'h-margin-b-none h-margin-b-tiny h-text-bold Heading__StyledHeading-sc-6yiixr-0 jHwOVX'})
-name_text = name.get_text()
+name_text = name.get_text().strip()
 print(name_text)
 
 #scraps image for item
