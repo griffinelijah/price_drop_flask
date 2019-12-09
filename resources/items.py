88accf8a23c16517e378e280e5fa3b0cde664b65
Griffin Delgado <griffinelijahd@gmail.com>
2019-12-09 11:10:57 -0600

    refactored item create for farfetch items now working properly

--- resources/items.py
+++ resources/items.py
@@ -2,7 +2,10 @@ import models
 from flask import Blueprint, jsonify, request
 from playhouse.shortcuts import model_to_dict
 from flask_login import current_user, login_required
+import bs4 as bs 
+import urllib.request
 
+#import data stored to variables from scraping scripts and use that to create item. use payload['url'] to scrape the page and use the scraped data to create an item here instead of in on initialize in models.py
 items = Blueprint('items', 'items')
 
 #create route for items
@@ -12,20 +15,51 @@ def create_item(listId):
 	payload = request.get_json()
 	#query for List to add item to
 	found_list = models.List.get_by_id(listId)
+
+	farfetch_url = (payload['url'])
+
+	sauce = urllib.request.urlopen(farfetch_url).read()
+
+	#Load the plain html data into a variable
+	soup = bs.BeautifulSoup(sauce, 'html.parser')
+
+	#this gets the original price for and if applicable discounted price of an item on farfetch
+	# orig_price = soup.select('#slice-pdp > div > div._c84e0a > div._715521 > div._844eda > div > span._ffbca2._1fe24a')
+	orig_price = soup.find('span', {'class': '_ffbca2 _1fe24a'})
+	print('\nthis is the originial price for a farfetch itiem')
+	print(orig_price)
+	farfetch_orig_price_text = orig_price.get_text().strip()
+	print('\nthis is the orig price get_text')
+	print(farfetch_orig_price_text)
+	# print(orig_price)
+
+	#discounted price 
+	disc_price = soup.select('#slice-pdp > div > div._c84e0a > div._715521 > div._844eda > div > strong')
+	farfetch_disc_price_text = disc_price[0].get_text().strip()
+	print(farfetch_disc_price_text)
+
+	#this will get the name of the item
+	name = soup.find('span', {'class': '_b4693b'})
+	farfetch_name_text = name.get_text().strip()
+	print(farfetch_name_text)
+
+	#this will get the image src for item
+	image = soup.find('img', {'class': '_221e30'})
+	farfetch_image_src = image['src']
+	print(farfetch_image_src)
+
+	item_object_farfetch = models.Item(url = farfetch_url, name = farfetch_name_text, image =farfetch_image_src, original_price = farfetch_orig_price_text, disc_price = farfetch_disc_price_text, notif_preference = '25')
+	#turn into dict before creating record in db
+	item_dict_farfetch = model_to_dict(item_object_farfetch)
 	#create the new item with payload info
 	item = models.Item.create(
-		url = payload['url'],
-		image = payload['image'],
-		original_price = payload['original_price'],
-		disc_price = payload['disc_price'],
-		notif_preference = payload['notif_preference'],
-		list_id = found_list.id
+		**item_dict_farfetch
 	)
 
 	#turn to dict before sending response
 	item_dict = model_to_dict(item)
 	print(item_dict)
-	return jsonify(data=item_dict, status={'code': 201, 'message': "Successfully created item"}), 201
+	return jsonify(data=str(item_dict), status={'code': 201, 'message': "Successfully created item"}), 201
 
 #get a lists items
 @items.route('/<listId>', methods=['GET'])
--- scraping_scripts/farfetch.py
+++ scraping_scripts/farfetch.py
@@ -4,33 +4,35 @@ import urllib.request
 import sqlite3
 conn = sqlite3.connect('price_drop.sqlite', check_same_thread=False)
 curs = conn.cursor()
+from resources.items import *
 
 #this is the Url we will scrape / will later change to a variable that will hold url user inputs
-farfetch_url = ('https://www.farfetch.com/shopping/men/a-cold-wall-faded-effect-logo-print-hoodie-item-13790443.aspx?storeid=9306')
-sauce = urllib.request.urlopen(farfetch_url).read()
+# farfetch_url = (payload['url'])
 
-#Load the plain html data into a variable
-soup = bs.BeautifulSoup(sauce, 'html.parser')
+# sauce = urllib.request.urlopen(farfetch_url).read()
 
-#this gets the original price for and if applicable discounted price of an item on farfetch
-orig_price = soup.select('#slice-pdp > div > div._c84e0a > div._715521 > div._844eda > div > span._ffbca2._1fe24a')
-farfetch_orig_price_text = orig_price[0].get_text().strip()
-print(farfetch_orig_price_text)
+# #Load the plain html data into a variable
+# soup = bs.BeautifulSoup(sauce, 'html.parser')
 
-#discounted price 
-disc_price = soup.select('#slice-pdp > div > div._c84e0a > div._715521 > div._844eda > div > strong')
-farfetch_disc_price_text = disc_price[0].get_text().strip()
-print(farfetch_disc_price_text)
+# #this gets the original price for and if applicable discounted price of an item on farfetch
+# orig_price = soup.select('#slice-pdp > div > div._c84e0a > div._715521 > div._844eda > div > span._ffbca2._1fe24a')
+# farfetch_orig_price_text = orig_price[0].get_text().strip()
+# print(farfetch_orig_price_text)
 
-#this will get the name of the item
-name = soup.find('span', {'class': '_b4693b'})
-farfetch_name_text = name.get_text().strip()
-print(farfetch_name_text)
+# #discounted price 
+# disc_price = soup.select('#slice-pdp > div > div._c84e0a > div._715521 > div._844eda > div > strong')
+# farfetch_disc_price_text = disc_price[0].get_text().strip()
+# print(farfetch_disc_price_text)
 
-#this will get the image src for item
-image = soup.find('img', {'class': '_221e30'})
-farfetch_image_src = image['src']
-print(farfetch_image_src)
+# #this will get the name of the item
+# name = soup.find('span', {'class': '_b4693b'})
+# farfetch_name_text = name.get_text().strip()
+# print(farfetch_name_text)
+
+# #this will get the image src for item
+# image = soup.find('img', {'class': '_221e30'})
+# farfetch_image_src = image['src']
+# print(farfetch_image_src)
 
 
 
